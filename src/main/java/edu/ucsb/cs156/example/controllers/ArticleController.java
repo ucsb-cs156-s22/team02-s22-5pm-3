a4a43bd589460c13688bc7b5d6ce22fe81dbc6db
Aashay Parab <aashay_parab@ucsb.edu>
2022-05-01 01:57:25 -0700

    Tests and controller added

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticleController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticleController.java
@@ -40,6 +40,17 @@ public class ArticleController extends ApiController {
         return articles;
     }
 
+    @ApiOperation(value = "Get a single article")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public Article getById(
+            @ApiParam("id of review to be viewed") @RequestParam Long id) {
+            Article article = articleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Article.class, id));
+
+        return article;
+    }
+
     @ApiOperation(value = "Create a Article")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
@@ -62,5 +73,39 @@ public class ArticleController extends ApiController {
 
         return savedArticle;
     }
+
+    @ApiOperation(value = "Delete an article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteArticle(
+            @ApiParam("id") @RequestParam Long id) {
+        Article article = articleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Article.class, id));
+
+        articleRepository.delete(article);
+        return genericMessage("Article with id %s deleted".formatted(id));
+    }
+
+    @ApiOperation(value = "Update a single article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public Article updateArticle(
+            @ApiParam("id of article to be updated") @RequestParam Long id,
+            @RequestBody @Valid Article incoming) {
+
+        Article article = articleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Article.class, id));
+
+        article.setTitle(incoming.getTitle());
+        article.setUrl(incoming.getUrl());
+        article.setExplanation(incoming.getExplanation());
+        article.setEmail(incoming.getEmail());
+        article.setDateAdded(incoming.getDateAdded());
+
+        articleRepository.save(article);
+
+        return article;
+    }
+
     
 }
